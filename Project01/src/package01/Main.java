2ac3d579963d71cf98b4a62d1d81d447585e5a5a
Andrijana Ristic <andrijanaristic28@gmail.com>
2022-02-25 17:54:28 +0100

    new project01

---
+++ Project01/src/package01/Main.java
@@ -0,0 +1,16 @@
+package package01;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String,Double> maps=new HashMap<>();
+        maps.put("Mika",10.0);
+        maps.put("Janja",9.8);
+        maps.put("Joka",9.9);
+
+        for(String key : maps.keySet())
+            System.out.println(key+": "+maps.get(key));
+    }
+}
